f645324dcc911d3173b41a886fd3bcec6d64f1e2
nhanphan <hncchannel@gmail.com>
2022-10-19 17:10:38 +0700

    Hien thi danh sach cac contact lay ve tu server

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const config = {
-  app: {
-    port: process.env.PORT || 3000,
+const app = require("./app");
+const config = require("./app/config");
+const MongoDB = require(".app/utils/mongodb.util");
+
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log(`Connected to the database`);
+
+    const PORT = config.app.port;
+    app.listen(PORT , () => {
+      console.Console.log(`Server is running on port ${PORT}`);
+    })
+  } catch (error) {
+    Console.log("Cannot connect to the database!" , error);
+    process.exit();
   }
-};
-module.exports = config;
+}
+
+
+  startServer();
